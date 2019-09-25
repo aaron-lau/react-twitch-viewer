5a528832c55f2502b4b64814d181678620602b27
Aaron Lau <aaron.lau@datadoghq.com>
2019-09-25 17:36:41 -0400

    Updates

---
+++ precache-manifest.9b26d1c9c393c07d3d1ff96f364a1e0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee0321fbe92244b6baa087c44145e31",
+    "url": "/react-twitch-viewer/index.html"
+  },
+  {
+    "revision": "a1382a05baf0cd682c8c",
+    "url": "/react-twitch-viewer/static/css/2.70a01efe.chunk.css"
+  },
+  {
+    "revision": "e9e45f891b5ff52d02a8",
+    "url": "/react-twitch-viewer/static/css/main.6d65d191.chunk.css"
+  },
+  {
+    "revision": "a1382a05baf0cd682c8c",
+    "url": "/react-twitch-viewer/static/js/2.0d325d93.chunk.js"
+  },
+  {
+    "revision": "e9e45f891b5ff52d02a8",
+    "url": "/react-twitch-viewer/static/js/main.9388701c.chunk.js"
+  },
+  {
+    "revision": "ccb2ca1ab460103c87d3",
+    "url": "/react-twitch-viewer/static/js/runtime~main.5947e675.js"
+  }
+]);
